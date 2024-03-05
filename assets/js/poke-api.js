79555bfa5945a0abe31076e19a1ff381671db20a
Alochio <vinicius.alochio@aluno.ufop.edu.br>
2024-03-05 14:59:37 -0300

    pegando todas as informações e imagens da API pronto

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeApi = {};
 
-pokeApi.getPokemons = (offset = 10, limit = 10) => {
+pokeApi.getPokemonDatail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+};
+
+pokeApi.getPokemons = (offset = 1, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error)) 
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDatail))
+    .then((detailRequest) => Promise.all(detailRequest))
+    .then((pokemonsDetails) => pokemonsDetails);
 };
