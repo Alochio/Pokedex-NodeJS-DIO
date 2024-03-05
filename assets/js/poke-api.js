02956ba9cf36c122814118831bd5a5841500d408
Alochio <vinicius.alochio@aluno.ufop.edu.br>
2024-03-05 16:19:04 -0300

    criando model pokemon -> facilitar nas chamadas

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,20 @@
 const pokeApi = {};
 
+function convertPokeApiDatailToPokemon(pokeDatail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDatail.order;
+  pokemon.name = pokeDatail.name;
+  pokemon.types = pokeDatail.types.map((typeSlot) => typeSlot.type.name);
+  pokemon.type = pokemon.types.length > 0 ? pokemon.types[0] : '';
+  pokemon.photo = pokeDatail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
+
 pokeApi.getPokemonDatail = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDatailToPokemon);
 };
 
 pokeApi.getPokemons = (offset = 1, limit = 10) => {
