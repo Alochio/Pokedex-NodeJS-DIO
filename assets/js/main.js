70761b1985c26b3f38926ecf0541886a0416b883
Alochio <vinicius.alochio@aluno.ufop.edu.br>
2024-03-06 16:29:00 -0300

    modal dinamico funcionando para todos os pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,19 @@ const loadMoreButton = document.getElementById("loadMoreButton");
 const maxRecords = 200; //Limite máximo de exibir pokemons
 const limit = 10;
 let offset = 0;
+let pokemons = []; // Adicione esta linha
 
-function createModalOpener(pokemon) {
-  
-  return function () {
+function createModalOpener() { // Remova o argumento 'pokemon'
+  return function (event) {
+    var index = event.currentTarget.dataset.index;
+    var pokemon = pokemons[index]; // Agora 'pokemons' é acessível aqui
+    
     var modalElement = document.getElementById("exampleModal");
     var modal = new bootstrap.Modal(modalElement);
 
     var modalTitle = document.getElementById("modalName");
     modalTitle.textContent = pokemon.name;
-    
+
     var modalImg = modalElement.getElementsByTagName("img")[0];
     modalImg.src = pokemon.photo;
 
@@ -24,11 +27,9 @@ function createModalOpener(pokemon) {
     modalSecondaryType.textContent = pokemon.types[1]
       ? "Tipo secundário: " + pokemon.types[1]
       : "";
-    
+
     var pokemonType = pokemon.types[0];
     var modalElement = document.getElementById("modalDetails");
-
-
 
     // Lista de todas as classes de tipo de Pokémon
     var pokemonTypes = [
@@ -52,8 +53,6 @@ function createModalOpener(pokemon) {
       "fairy",
     ];
 
-    
-
     // Remove todas as classes de tipo de Pokémon
     for (var i = 0; i < pokemonTypes.length; i++) {
       modalElement.classList.remove(pokemonTypes[i]);
@@ -61,20 +60,18 @@ function createModalOpener(pokemon) {
 
     // Adiciona a classe do tipo de Pokémon atual
     modalElement.classList.add(pokemonType);
-    console.log(pokemonType);
-
+    
     modal.show();
-
-    console.log(passei);
   };
 }
 
 function loadPokemonItens(offset, limit) {
-  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const newHtml = pokemons
+  pokeApi.getPokemons(offset, limit).then((newPokemons = []) => {
+    pokemons = pokemons.concat(newPokemons); // Concatene 'newPokemons' à variável 'pokemons' existente
+    const newHtml = newPokemons
       .map(
-        (pokemon) => `
-             <li class="pokemon openModal ${pokemon.type}">
+        (pokemon, index) => `
+             <li class="pokemon openModal ${pokemon.type}" data-index="${index + offset}">
                   <span class="number">#${pokemon.number}</span>
                   <span class="name">${pokemon.name}</span>
                   <div class="detail">
@@ -99,12 +96,11 @@ function loadPokemonItens(offset, limit) {
     var openModalElements = document.querySelectorAll(".openModal");
 
     // Adicione o evento de clique a cada elemento
-    openModalElements.forEach((element, index) => {
-      element.addEventListener("click", createModalOpener(pokemons[index]));
+    openModalElements.forEach((element) => {
+      element.addEventListener("click", createModalOpener());
     });
   });
 }
-
 
 loadPokemonItens(offset, limit);
 
@@ -120,4 +116,3 @@ loadMoreButton.addEventListener("click", () => {
     loadPokemonItens(offset, limit);
   }
 });
-
