aca1dfdd17cdd68900ec46bda6c992fc50a40caf
Alochio <vinicius.alochio@aluno.ufop.edu.br>
2024-03-06 15:01:18 -0300

    os primeiros a serem exibidos estão dinânicos -> segunda lista com erro

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,72 @@ const pokemonList = document.getElementById(`pokemonList`);
 const loadMoreButton = document.getElementById("loadMoreButton");
 
 const maxRecords = 200; //Limite máximo de exibir pokemons
-const limit = 12;
+const limit = 10;
 let offset = 0;
+
+function createModalOpener(pokemon) {
+  
+  return function () {
+    var modalElement = document.getElementById("exampleModal");
+    var modal = new bootstrap.Modal(modalElement);
+
+    var modalTitle = document.getElementById("modalName");
+    modalTitle.textContent = pokemon.name;
+    
+    var modalImg = modalElement.getElementsByTagName("img")[0];
+    modalImg.src = pokemon.photo;
+
+    var modalType = modalElement.getElementsByTagName("p")[0];
+    modalType.textContent = "Tipo principal: " + pokemon.types[0];
+
+    var modalSecondaryType = modalElement.getElementsByTagName("p")[1];
+    modalSecondaryType.textContent = pokemon.types[1]
+      ? "Tipo secundário: " + pokemon.types[1]
+      : "";
+    
+    var pokemonType = pokemon.types[0];
+    var modalElement = document.getElementById("modalDetails");
+
+
+
+    // Lista de todas as classes de tipo de Pokémon
+    var pokemonTypes = [
+      "normal",
+      "grass",
+      "fire",
+      "water",
+      "electric",
+      "ice",
+      "ground",
+      "flying",
+      "poison",
+      "fighting",
+      "psychic",
+      "dark",
+      "rock",
+      "bug",
+      "ghost",
+      "steel",
+      "dragon",
+      "fairy",
+    ];
+
+    
+
+    // Remove todas as classes de tipo de Pokémon
+    for (var i = 0; i < pokemonTypes.length; i++) {
+      modalElement.classList.remove(pokemonTypes[i]);
+    }
+
+    // Adiciona a classe do tipo de Pokémon atual
+    modalElement.classList.add(pokemonType);
+    console.log(pokemonType);
+
+    modal.show();
+
+    console.log(passei);
+  };
+}
 
 function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
@@ -31,17 +95,16 @@ function loadPokemonItens(offset, limit) {
       .join("");
     pokemonList.innerHTML += newHtml;
 
-    // Adiciona um evento de clique a cada elemento
-    var openModalElements = document.getElementsByClassName("openModal");
-    for (var i = 0; i < openModalElements.length; i++) {
-      openModalElements[i].addEventListener("click", function () {
-        var modalElement = document.getElementById("exampleModal");
-        var modal = new bootstrap.Modal(modalElement);
-        modal.show();
-      });
-    }
+    // Obtenha todos os elementos abertos do modal
+    var openModalElements = document.querySelectorAll(".openModal");
+
+    // Adicione o evento de clique a cada elemento
+    openModalElements.forEach((element, index) => {
+      element.addEventListener("click", createModalOpener(pokemons[index]));
+    });
   });
 }
+
 
 loadPokemonItens(offset, limit);
 
@@ -52,9 +115,9 @@ loadMoreButton.addEventListener("click", () => {
   if (qtdRecordsWithNexPage >= maxRecords) {
     const newLimit = maxRecords - offset;
     loadPokemonItens(offset, limit);
-
     //loadMoreButton.parentElement.removeChild(loadMoreButton); -> Tirando a função de limite máximo (podemos ver todos os pokemons da API)
   } else {
     loadPokemonItens(offset, limit);
   }
 });
+
